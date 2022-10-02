d78453a8303b6446b3978932fe07b99ee39b81c0
Bereket Terefe <berelliant11@gmail.com>
2022-10-03 00:42:28 +0300

    [feat] - footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.scss';
 import Navbar from './components/Navbar';
 import NotFound from './components/404';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App light">
       <Navbar />
       <NotFound />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <div className='pp'>
+            <span>Privacy Policy</span>
+        </div>
+        <div className='copyright'>
+            <span>© 2022 rav3n11</span>
+        </div>
+    </div>
+  )
+}
